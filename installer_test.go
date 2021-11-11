3355c377f602eff74eafc8ceebf656d5c1f9fb4b
Radek Simko <radek.simko@gmail.com>
2021-11-11 12:10:33 +0000

    installer: add E2E test for the public API too

--- installer_test.go
+++ installer_test.go
@@ -1,1 +1,29 @@
 package install
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/hc-install/internal/testutil"
+	"github.com/hashicorp/hc-install/product"
+	"github.com/hashicorp/hc-install/releases"
+	"github.com/hashicorp/hc-install/src"
+)
+
+func TestInstaller_Ensure(t *testing.T) {
+	testutil.EndToEndTest(t)
+
+	// most of this logic is already tested within individual packages
+	// so this is just a simple E2E test to ensure the public API
+	// also works and continues working
+
+	i := NewInstaller()
+	_, err := i.Ensure(context.Background(), []src.Source{
+		&releases.LatestVersion{
+			Product: product.Terraform,
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}

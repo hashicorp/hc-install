6c4cca6f252391fc227828af26483f43846e5dc2
Radek Simko <radek.simko@gmail.com>
2021-11-30 17:01:56 +0100

    fs.AnyVersion: Introduce ExactBinPath (#40)
    * fs.AnyVersion: Introduce ExactBinPath
    
    * Update fs/any_version.go
    
    Co-authored-by: Brian Flad <bflad417@gmail.com>
    
    Co-authored-by: Brian Flad <bflad417@gmail.com>

--- fs/any_version.go
+++ fs/any_version.go
@@ -12,12 +12,28 @@ import (
 	"github.com/hashicorp/hc-install/product"
 )
 
-// AnyVersion finds the first executable binary of the product name
-// within system $PATH and any declared ExtraPaths
-// (which are *appended* to any directories in $PATH)
+// AnyVersion finds an executable binary of any version
+// either defined by ExactBinPath, or as part of Product.
+//
+// When ExactBinPath is used, the source is skipped when
+// the binary is not found or accessible/executable.
+//
+// When Product is used, binary name is looked up within system $PATH
+// and any declared ExtraPaths (which are *appended* to
+// any directories in $PATH). Source is skipped if no binary
+// is found or accessible/executable.
 type AnyVersion struct {
-	Product    product.Product
+	// Product represents the product (its binary name to look up),
+	// conflicts with ExactBinPath
+	Product *product.Product
+
+	// ExtraPaths represents additional dir paths to be appended to
+	// the default system $PATH, conflicts with ExactBinPath
 	ExtraPaths []string
+
+	// ExactBinPath represents exact path to the binary,
+	// conflicts with Product and ExtraPaths
+	ExactBinPath string
 
 	logger *log.Logger
 }
@@ -27,6 +43,12 @@ func (*AnyVersion) IsSourceImpl() src.InstallSrcSigil {
 }
 
 func (av *AnyVersion) Validate() error {
+	if av.ExactBinPath != "" && (av.Product != nil || len(av.ExtraPaths) > 0) {
+		return fmt.Errorf("use either ExactBinPath or Product + ExtraPaths, not both")
+	}
+	if !filepath.IsAbs(av.ExactBinPath) {
+		return fmt.Errorf("expected ExactBinPath (%q) to be an absolute path", av.ExactBinPath)
+	}
 	if !validators.IsBinaryNameValid(av.Product.BinaryName()) {
 		return fmt.Errorf("invalid binary name: %q", av.Product.BinaryName())
 	}
@@ -45,6 +67,15 @@ func (av *AnyVersion) log() *log.Logger {
 }
 
 func (av *AnyVersion) Find(ctx context.Context) (string, error) {
+	if av.ExactBinPath != "" {
+		err := checkExecutable(av.ExactBinPath)
+		if err != nil {
+			return "", errors.SkippableErr(err)
+		}
+
+		return av.ExactBinPath, nil
+	}
+
 	execPath, err := findFile(lookupDirs(av.ExtraPaths), av.Product.BinaryName(), checkExecutable)
 	if err != nil {
 		return "", errors.SkippableErr(err)
--- fs/fs_unix_test.go
+++ fs/fs_unix_test.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/hashicorp/hc-install/errors"
 	"github.com/hashicorp/hc-install/internal/testutil"
 	"github.com/hashicorp/hc-install/product"
 )
@@ -26,7 +27,7 @@ func TestAnyVersion_notExecutable(t *testing.T) {
 	os.Setenv("PATH", dirPath)
 
 	av := &AnyVersion{
-		Product: product.Product{
+		Product: &product.Product{
 			BinaryName: func() string { return fileName },
 		},
 	}
@@ -56,7 +57,7 @@ func TestAnyVersion_executable(t *testing.T) {
 	}
 
 	av := &AnyVersion{
-		Product: product.Product{
+		Product: &product.Product{
 			BinaryName: func() string { return fileName },
 		},
 	}
@@ -64,5 +65,49 @@ func TestAnyVersion_executable(t *testing.T) {
 	_, err = av.Find(context.Background())
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestAnyVersion_exactBinPath(t *testing.T) {
+	testutil.EndToEndTest(t)
+
+	dirPath, fileName := createTempFile(t, "")
+	fullPath := filepath.Join(dirPath, fileName)
+	err := os.Chmod(fullPath, 0700)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	av := &AnyVersion{
+		ExactBinPath: fullPath,
+	}
+	av.SetLogger(testutil.TestLogger())
+	_, err = av.Find(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestAnyVersion_exactBinPath_notExecutable(t *testing.T) {
+	testutil.EndToEndTest(t)
+
+	dirPath, fileName := createTempFile(t, "")
+	fullPath := filepath.Join(dirPath, fileName)
+	err := os.Chmod(fullPath, 0600)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	av := &AnyVersion{
+		ExactBinPath: fullPath,
+	}
+	av.SetLogger(testutil.TestLogger())
+	_, err = av.Find(context.Background())
+	if err == nil {
+		t.Fatal("expected error for non-executable file")
+	}
+
+	if !errors.IsErrorSkippable(err) {
+		t.Fatalf("expected a skippable error, got: %#v", err)
 	}
 }

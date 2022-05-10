d615262197ecd2d3ed2f6048efd9c60ab69b5893
magodo <wztdyl@sina.com>
2022-05-10 21:08:59 +0800

    `fs.Version` to support version constrained binary discovery (#52)
    * `fs.AnyVersion` supports optional version constraint
    
    Allows users to optionally specify one or more (comma separated) version constraints for `fs.AnyVersion` source.
    
    * Refine readme
    
    * Modify `Constraint` type and name to align with `releases.Versions`
    
    * revert everything
    
    * New source - `fs.Version`: allows specifying version constraints
    
    * Modify e2e test
    
    * enable test
    
    * modify and pass test

--- fs/fs_test.go
+++ fs/fs_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/hc-install/internal/testutil"
 	"github.com/hashicorp/hc-install/product"
+	"github.com/hashicorp/hc-install/releases"
 	"github.com/hashicorp/hc-install/src"
 )
 
@@ -16,6 +17,9 @@ var (
 
 	_ src.Findable       = &ExactVersion{}
 	_ src.LoggerSettable = &ExactVersion{}
+
+	_ src.Findable       = &Version{}
+	_ src.LoggerSettable = &Version{}
 )
 
 func TestExactVersion(t *testing.T) {
@@ -34,5 +38,39 @@ func TestExactVersion(t *testing.T) {
 	_, err := ev.Find(context.Background())
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestVersion(t *testing.T) {
+	testutil.EndToEndTest(t)
+
+	ctx := context.Background()
+
+	p := t.TempDir()
+	t.Setenv("PATH", p)
+	ev := releases.ExactVersion{
+		Product:    product.Terraform,
+		Version:    version.Must(version.NewVersion("1.0.0")),
+		InstallDir: p,
+	}
+
+	if _, err := ev.Install(ctx); err != nil {
+		t.Fatalf("installing release version failed: %v", err)
+	}
+
+	// Version matches constraint
+	v := &Version{
+		Product:     product.Terraform,
+		Constraints: version.MustConstraints(version.NewConstraint(">= 1.0")),
+	}
+	v.SetLogger(testutil.TestLogger())
+	if _, err := v.Find(ctx); err != nil {
+		t.Fatalf("finding: %v", err)
+	}
+
+	// Version mismatches constraint
+	v.Constraints = version.MustConstraints(version.NewConstraint("> 1.0"))
+	if _, err := v.Find(ctx); err == nil {
+		t.Fatal("expecting error")
 	}
 }

51afd25a903c233b672f763c931e2e7e0029d47f
Tim Gross <tgross@hashicorp.com>
2022-05-20 09:34:45 -0400

    cli: make `hc-install install` a subcommand

---
+++ cmd/hc-install/cmd_install.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/hashicorp/go-version"
+	"github.com/mitchellh/cli"
+	"gophers.dev/pkgs/ignore"
+
+	hci "github.com/hashicorp/hc-install"
+	"github.com/hashicorp/hc-install/product"
+	"github.com/hashicorp/hc-install/releases"
+	"github.com/hashicorp/hc-install/src"
+)
+
+type InstallCommand struct {
+	Ui cli.Ui
+}
+
+func (c *InstallCommand) Name() string { return "install" }
+
+func (c *InstallCommand) Synopsis() string {
+	return "Install a HashiCorp product"
+}
+
+func (c *InstallCommand) Help() string {
+	helpText := `
+Usage: hc-install install [options] -version <version> <product>
+
+  This command installs a HashiCorp product.
+  Options:
+    -version  [REQUIRED] Version of product to install.
+    -path     Path to directory where the product will be installed. Defaults
+              to current working directory.
+`
+	return strings.TrimSpace(helpText)
+}
+
+func (c *InstallCommand) Run(args []string) int {
+
+	var (
+		version        string
+		installDirPath string
+	)
+
+	fs := flag.NewFlagSet("install", flag.ExitOnError)
+	fs.Usage = func() { c.Ui.Output(c.Help()) }
+	fs.StringVar(&version, "version", "", "version of product to install")
+	fs.StringVar(&installDirPath, "path", "", "path to directory where production will be installed")
+
+	if err := fs.Parse(args); err != nil {
+		return 1
+	}
+
+	// golang's arg parser is Posix-compliant but doesn't match the
+	// common GNU flag parsing argument, so force an error rather than
+	// silently dropping the options
+	args = fs.Args()
+	if len(args) != 1 {
+		c.Ui.Error(`This command requires one positional argument: <product>
+Option flags must be provided before the positional argument`)
+		return 1
+	}
+	product := fs.Args()[0]
+
+	if version == "" {
+		c.Ui.Error("-version flag is required")
+		return 1
+	}
+
+	if installDirPath == "" {
+		cwd, err := os.Getwd()
+		if err != nil {
+			c.Ui.Error(fmt.Sprintf("Could not get current working directory for default installation path: %v", err))
+		}
+		installDirPath = cwd
+		return 1
+	}
+
+	if err := c.install(product, version, installDirPath); err != nil {
+		msg := fmt.Sprintf("failed to install %s@%s: %v", product, version, err)
+		c.Ui.Error(msg)
+		return 1
+	}
+
+	return 0
+}
+
+func (c *InstallCommand) install(project, tag, installDirPath string) error {
+	msg := fmt.Sprintf("hc-install: will install %s@%s", project, tag)
+	c.Ui.Info(msg)
+
+	v := version.Must(version.NewVersion(tag))
+	i := hci.NewInstaller()
+
+	var source src.Source
+	switch project {
+	case "consul":
+		source = &releases.ExactVersion{
+			Product: product.Consul,
+			Version: v,
+		}
+	case "vault":
+		source = &releases.ExactVersion{
+			Product: product.Vault,
+			Version: v,
+		}
+	default:
+		return fmt.Errorf("project %s cannot be downloaded", project)
+	}
+
+	ctx := context.Background()
+	executable, instErr := i.Ensure(ctx, []src.Source{source})
+	if instErr != nil {
+		return instErr
+	}
+
+	if err := c.copyProgram(executable, installDirPath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *InstallCommand) copyProgram(programPath, installDirPath string) error {
+	program := filepath.Base(programPath)
+	destination := filepath.Join(installDirPath, program)
+
+	msg := fmt.Sprintf("hc-install: copy executable to %s", destination)
+	c.Ui.Info(msg)
+
+	return clone(programPath, destination)
+}
+
+func clone(source, destination string) error {
+	sFile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer ignore.Close(sFile)
+
+	dFile, err := os.OpenFile(destination, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0755)
+	if err != nil {
+		return err
+	}
+	defer ignore.Close(dFile)
+
+	_, err = io.Copy(dFile, sFile)
+	return err
+}
--- cmd/hc-install/main.go
+++ cmd/hc-install/main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"io"
 	"log"
 	"os"
-	"path/filepath"
 
-	"github.com/hashicorp/go-version"
-	hci "github.com/hashicorp/hc-install"
-	"github.com/hashicorp/hc-install/product"
-	"github.com/hashicorp/hc-install/releases"
-	"github.com/hashicorp/hc-install/src"
 	"github.com/hashicorp/logutils"
 	"github.com/mitchellh/cli"
-	"gophers.dev/pkgs/extractors/env"
-	"gophers.dev/pkgs/ignore"
 )
 
 func main() {
@@ -37,102 +26,20 @@ func main() {
 		},
 	}
 
-	exitStatus := run(ui, os.Args[1:])
+	c := cli.NewCLI("hc-install", "0.0.0")
+	c.Args = os.Args[1:]
+	c.Commands = map[string]cli.CommandFactory{
+		"install": func() (cli.Command, error) {
+			return &InstallCommand{
+				Ui: ui,
+			}, nil
+		},
+	}
+
+	exitStatus, err := c.Run()
+	if err != nil {
+		ui.Error(err.Error())
+	}
 
 	os.Exit(exitStatus)
-}
-
-func run(ui cli.Ui, args []string) int {
-	if len(args) != 2 {
-		ui.Error("usage: hc-install PRODUCT VERSION")
-		return 1
-	}
-
-	if err := install(ui, args[0], args[1]); err != nil {
-		msg := fmt.Sprintf("failed to install %s@%s: %v", args[0], args[1], err)
-		ui.Error(msg)
-		return 1
-	}
-
-	return 0
-}
-
-func install(ui cli.Ui, project, tag string) error {
-	msg := fmt.Sprintf("hc-install: will install %s@%s", project, tag)
-	ui.Info(msg)
-
-	v := version.Must(version.NewVersion(tag))
-	i := hci.NewInstaller()
-
-	var source src.Source
-	switch project {
-	case "consul":
-		source = &releases.ExactVersion{
-			Product: product.Consul,
-			Version: v,
-		}
-	case "vault":
-		source = &releases.ExactVersion{
-			Product: product.Vault,
-			Version: v,
-		}
-	default:
-		return fmt.Errorf("project %s cannot be downloaded", project)
-	}
-
-	ctx := context.Background()
-	executable, instErr := i.Ensure(ctx, []src.Source{source})
-	if instErr != nil {
-		return instErr
-	}
-
-	if err := copyProgram(ui, executable); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func copyProgram(ui cli.Ui, programPath string) error {
-	var (
-		gobin   string
-		gopath  string
-		program = filepath.Base(programPath)
-	)
-	if err := env.ParseOS(env.Schema{
-		"GOBIN":  env.String(&gobin, false),
-		"GOPATH": env.String(&gopath, false),
-	}); err != nil {
-		return err
-	}
-
-	var destination string
-	switch {
-	case gobin != "":
-		destination = filepath.Join(gobin, program)
-	case gopath != "":
-		destination = filepath.Join(gopath, "bin", program)
-	}
-
-	msg := fmt.Sprintf("hc-install: copy executable to %s", destination)
-	ui.Info(msg)
-
-	return clone(programPath, destination)
-}
-
-func clone(source, destination string) error {
-	sFile, err := os.Open(source)
-	if err != nil {
-		return err
-	}
-	defer ignore.Close(sFile)
-
-	dFile, err := os.OpenFile(destination, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0755)
-	if err != nil {
-		return err
-	}
-	defer ignore.Close(dFile)
-
-	_, err = io.Copy(dFile, sFile)
-	return err
 }
